753b5bb51524459add637862793e5c46cced76d4
Yannick Schwartz <yschwart@drago.saclay.inria.fr>
2014-04-04 16:40:30 +0200

    preproc_all for drago

--- preproc_all.py
+++ preproc_all.py
@@ -3,15 +3,19 @@ import glob
 
 module_dir = os.path.dirname(os.path.abspath(__file__))
 
-data_dir = '/neurospin/tmp/brainpedia/data'
-base_out_dir = '/neurospin/tmp/brainpedia/new_preproc'
-exec_path = '/home/ys218403/Python/lib/pypreprocess/examples/openfmri_preproc.py'
+data_dir = '/storage/data/openfmri'
+base_out_dir = '/storage/workspace/yschwart/new_preproc'
+exec_path = '/storage/workspace/yschwart/python/pypreprocess/examples/openfmri_preproc.py'
 
 for input_dir in glob.glob(data_dir + '/*'):
     print 'input_dir:', input_dir
     job = {}
 
     study_id = os.path.split(input_dir)[1]
+    if study_id == 'ds113':
+        print 'skip'
+        continue
+
     output_dir = base_out_dir + '/%s' % study_id
     print 'output_dir:', output_dir
     if not os.path.exists(output_dir):
@@ -31,4 +35,4 @@ for input_dir in glob.glob(data_dir + '/*'):
     with open(os.path.join(job_dir, 'job.qsub'), 'wb') as f:
         f.write(qsub)
     os.chmod(os.path.join(job_dir, 'job.qsub'), 0777)
-    os.system('qsub %s' % (os.path.join(job_dir, 'job.qsub')))
+    os.system('. %s' % (os.path.join(job_dir, 'job.qsub')))
