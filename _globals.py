b03b9f88d8cf8de4e622037c5eab026473fb4fb0
Yannick Schwartz <yschwart@drago.saclay.inria.fr>
2014-04-06 19:25:16 +0200

    adding _globals with std target affine and shape

---
+++ _globals.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+target_affine = np.array([[-3., 0.,  0., 78.],
+                          [0., 3., 0., -111.],
+                          [0., 0., 3., -51.],
+                          [0., 0., 0., 1.]])
+
+target_shape = (53, 63, 46)
