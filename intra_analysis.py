276709a938b443447a4bd2b5d5d251b4c62a7264
Yannick Schwartz <ys218403@is220245.(none)>
2014-04-10 19:48:21 +0200

    fixing ds005

--- intra_analysis.py
+++ intra_analysis.py
@@ -175,7 +175,7 @@ if __name__ == '__main__':
 
     # for study_dir in globing(root_dir, '*'):
     #     study_id = os.path.split(study_dir)[1]
-    for study_id in ['ds002']:
+    for study_id in ['ds005']:
         print study_id
 
         infos = glob_subjects_dirs('%s/%s/sub???' % (root_dir, study_id))
