883edb0053e13459cd84122abdef638f46df1033
Yannick Schwartz <yschwart@drago.saclay.inria.fr>
2014-04-14 14:11:21 +0200

    ds110

--- intra_analysis.py
+++ intra_analysis.py
@@ -183,7 +183,7 @@ if __name__ == '__main__':
 
     # for study_dir in globing(root_dir, '*'):
     #     study_id = os.path.split(study_dir)[1]
-    for study_id in ['vagharchakian2012temporal']:
+    for study_id in ['ds110']:
         print study_id
 
         infos = glob_subjects_dirs('%s/%s/sub???' % (root_dir, study_id))
