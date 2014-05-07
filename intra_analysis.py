1c77a62f15b152cf23c7e3ae57a8810b51c29053
Yannick Schwartz <yannick.schwartz@cea.fr>
2014-05-07 16:30:41 +0200

    gogogo

--- intra_analysis.py
+++ intra_analysis.py
@@ -175,7 +175,7 @@ if __name__ == '__main__':
     from nignore.spm import IntraEncoder
     from nignore.utils import globing
 
-    n_jobs = 1
+    n_jobs = 24
 
     root_dir = '/storage/workspace/yschwart/new_brainpedia/preproc'
     result_dir = '/storage/workspace/yschwart/new_brainpedia/intra_stats_3mm'
