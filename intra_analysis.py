f5b4ea29d7c702ecebf6dbbef3de0002ea1abead
Yannick Schwartz <yschwart@drago.saclay.inria.fr>
2014-08-04 11:41:41 +0200

    glm

--- intra_analysis.py
+++ intra_analysis.py
@@ -8,11 +8,12 @@ from nipy.modalities.fmri.glm import GeneralLinearModel
 from nilearn.input_data import NiftiMasker, MultiNiftiMasker
 from nilearn.image import resample_img
 from joblib import Memory, Parallel, delayed
-
+from brainlet.utils import load_std_niimg
 
 target_affine = nb.load('mask_3mm.nii.gz').get_affine()
 target_shape = nb.load('mask_3mm.nii.gz').shape
-
+target_affine = None
+target_shape = None
 
 class IntraLinearModel(object):
 
@@ -47,6 +48,8 @@ class IntraLinearModel(object):
             if not session_data is None and not design_matrix is None)
         self.glm_ = [r[0] for r in all_results]
         self.design_mask_ = [r[1] for r in all_results]
+        print 'n_none:', sum([1 for g in self.glm_ if g is None])
+        print 'n_zero:', sum([np.sum(~m) for m in self.design_mask_])
         return self
 
     def check_design(self, design_matrices):
@@ -128,6 +131,7 @@ class IntraLinearModel(object):
 
 def _fit_glm(X, Y, glm_model, model_tol=1e10):
     design_mask = ~np.all(X == 0, axis=0)
+    design_mask = np.ones(X.shape[1], dtype=np.bool)
     sv = np.linalg.svd(X[:, design_mask])[1]
     sv = sv[0] / sv[-1]
     if sv < model_tol:
@@ -170,100 +174,105 @@ def do_intra_analysis(masker, output_dir, niimgs, design_matrices, contrasts):
 
 
 if __name__ == '__main__':
+    import glob
     from nignore.openfmri import Loader, glob_subjects_dirs
     from nignore.spm import IntraEncoder
     from nignore.utils import globing
 
-    n_jobs = 24
+    from load_data.openfmri import collect_openfmri, fetch_glm_data
 
-    root_dir = '/storage/workspace/yschwart/new_brainpedia/preproc'
-    result_dir = '/storage/workspace/yschwart/new_brainpedia/intra_stats_3mm_clean'
+    memory = Memory('/storage/workspace/yschwart/cache')
+    base_dir = '/storage/workspace/yschwart/new_brainpedia'
+    result_dir = '/storage/workspace/yschwart/new_brainpedia/youpla'
+    n_jobs = -1
 
-    loader = Loader(model_id='model001')
-    encoder = IntraEncoder()
+    # glob preproc folders                                                                                                                    
+    study_dirs = sorted(glob.glob(os.path.join(base_dir, 'preproc', '*')))
+    datasets, structural, functional, conditions, _ = collect_openfmri(study_dirs, memory=memory, n_jobs=-1)
 
-    masker = MultiNiftiMasker(mask='mask_3mm.nii.gz', standardize=True,
-                              smoothing_fwhm=6, n_jobs=1)
+    # we can filter the dataframes!                                                                                                           
+    functional = functional[functional.study == 'amalric2012mathematicians']
+    conditions = conditions[conditions.study == 'amalric2012mathematicians']
 
-    def one_map_per_run(contrasts):
-        angry_contrasts = {}
-        for contrast_id in contrasts:
-            n_sessions = len(contrasts[contrast_id])
-            run_id = 1
-            task_id, con_name = contrast_id.split('_', 1)
-            for i, session_con in enumerate(contrasts[contrast_id]):
-                is_null = np.all(np.array(session_con == 0))
-                if session_con is not None and not is_null:
-                    new_con_id = '%s_run%03i_%s' % (task_id, run_id, con_name)
-                    angry_contrasts[new_con_id] = [None] * n_sessions
-                    angry_contrasts[new_con_id][i] = session_con
-                    run_id += 1
-        return angry_contrasts
+    # computes design matrices for the given dataframes                                                                                       
+    designs = fetch_glm_data(datasets, functional, conditions, hrf_model='canonical with derivative', n_jobs=-1)
+    masker = MultiNiftiMasker(load_std_niimg('mask_1.5mm'), smoothing_fwhm=6, n_jobs=1)
 
-    def sanitize_contrast(contrasts, insert_derivative=True, per_run=False):
-        angry_contrasts = {}
-        for contrast_id in contrasts:
-            contrast = []
-            for session_con in contrasts[contrast_id]:
-                if session_con is not None:
-                    session_con = np.array(session_con)
-                    session_con = np.insert(
-                        session_con,
-                        np.arange(session_con.size) + 1, 0).tolist()
-                contrast.append(session_con)
-            angry_contrasts[contrast_id] = contrast
-        if per_run:
-            return one_map_per_run(angry_contrasts)
-        return angry_contrasts
+    # Compute contrasts
+    print 'Computing contrasts...'
 
-    for study_dir in globing(root_dir, 'ds*'):
-        study_id = os.path.split(study_dir)[1]
-    # for study_id in ['ds002']:
+    # for k in designs:
+    #     dm = designs[k]['design'][0]
 
-        infos = glob_subjects_dirs('%s/%s/sub???' % (root_dir, study_id))
-        docs = loader.fit_transform(infos['subjects_dirs'], infos['subjects'])
-        subjects_niimgs = encoder.fit_transform(docs, infos['subjects'])
-
-        # for i, subject_id in enumerate(infos['subjects']):
-        #     print subject_id
-        #     output_dir = '%s/%s/%s/%s/%s' % (result_dir, study_id, subject_id,
-        #                                      'model', 'model002')
-        #     niimgs = subjects_niimgs[i]
-        #     design_matrices = encoder.design_matrices_[i]
-        #     contrasts = docs[i]['contrasts']
-
-        #     # insert zeros in con_val because there is a derivative
-        #     angry_contrasts = sanitize_contrast(contrasts)
-        #     modeler = IntraLinearModel(
-        #         masker,
-        #         glm_model='ar1',
-        #         output_dir=output_dir,
-        #         target_affine=target_affine,
-        #         target_shape=target_shape,
-        #         output_effects=True,
-        #         output_variance=True,
-        #         n_jobs=n_jobs)
-        #     modeler.fit(niimgs, design_matrices)
-        #     modeler.contrast(angry_contrasts)
-
-        Parallel(n_jobs=n_jobs)(delayed(do_intra_analysis)(
-            masker=masker,
-            output_dir='%s/%s/%s/%s/%s' % (
-                result_dir, study_id, subject_id,
-                'model', 'model002'),
-            niimgs=subjects_niimgs[i],
-            design_matrices=encoder.design_matrices_[i],
-            contrasts=sanitize_contrast(docs[i]['contrasts'], per_run=False))
-            for i, subject_id in enumerate(infos['subjects'])
+    Parallel(n_jobs=-1)(delayed(do_intra_analysis)(
+        masker=masker,
+        output_dir='%s/%s/%s/%s/%s' % (
+            result_dir, k[0], k[1], 'model', 'model002'),
+            niimgs=designs[k]['bold'],
+            design_matrices=[dm.values for dm in designs[k]['design']],
+            contrasts=designs[k]['model001'])
+            for k in designs
         )
 
-        Parallel(n_jobs=n_jobs)(delayed(do_intra_analysis)(
-            masker=masker,
-            output_dir='%s/%s/%s/%s/%s' % (
-                result_dir, study_id, subject_id,
-                'model', 'model003'),
-            niimgs=subjects_niimgs[i],
-            design_matrices=encoder.design_matrices_[i],
-            contrasts=sanitize_contrast(docs[i]['contrasts'], per_run=True))
-            for i, subject_id in enumerate(infos['subjects'])
-        )
+    # n_jobs = 24
+
+    # root_dir = '/storage/workspace/yschwart/new_brainpedia/preproc'
+    # result_dir = '/storage/workspace/yschwart/new_brainpedia/intra_stats_3mm_clean'
+
+    # loader = Loader(model_id='model001')
+    # encoder = IntraEncoder()
+
+    # masker = MultiNiftiMasker(mask='mask_3mm.nii.gz', standardize=True,
+    #                           smoothing_fwhm=6, n_jobs=1)
+
+    # # for study_dir in globing(root_dir, 'ds*'):
+    # #     study_id = os.path.split(study_dir)[1]
+    # for study_id in ['amalric2012mathematicians']:
+
+    #     infos = glob_subjects_dirs('%s/%s/sub???' % (root_dir, study_id))
+    #     docs = loader.fit_transform(infos['subjects_dirs'], infos['subjects'])
+    #     subjects_niimgs = encoder.fit_transform(docs, infos['subjects'])
+
+    #     # for i, subject_id in enumerate(infos['subjects']):
+    #     #     print subject_id
+    #     #     output_dir = '%s/%s/%s/%s/%s' % (result_dir, study_id, subject_id,
+    #     #                                      'model', 'model002')
+    #     #     niimgs = subjects_niimgs[i]
+    #     #     design_matrices = encoder.design_matrices_[i]
+    #     #     contrasts = docs[i]['contrasts']
+
+    #     #     # insert zeros in con_val because there is a derivative
+    #     #     angry_contrasts = sanitize_contrast(contrasts)
+    #     #     modeler = IntraLinearModel(
+    #     #         masker,
+    #     #         glm_model='ar1',
+    #     #         output_dir=output_dir,
+    #     #         target_affine=target_affine,
+    #     #         target_shape=target_shape,
+    #     #         output_effects=True,
+    #     #         output_variance=True,
+    #     #         n_jobs=n_jobs)
+    #     #     modeler.fit(niimgs, design_matrices)
+    #     #     modeler.contrast(angry_contrasts)
+
+    #     Parallel(n_jobs=n_jobs)(delayed(do_intra_analysis)(
+    #         masker=masker,
+    #         output_dir='%s/%s/%s/%s/%s' % (
+    #             result_dir, study_id, subject_id,
+    #             'model', 'model002'),
+    #         niimgs=subjects_niimgs[i],
+    #         design_matrices=encoder.design_matrices_[i],
+    #         contrasts=sanitize_contrast(docs[i]['contrasts'], per_run=False))
+    #         for i, subject_id in enumerate(infos['subjects'])
+    #     )
+
+    #     Parallel(n_jobs=n_jobs)(delayed(do_intra_analysis)(
+    #         masker=masker,
+    #         output_dir='%s/%s/%s/%s/%s' % (
+    #             result_dir, study_id, subject_id,
+    #             'model', 'model003'),
+    #         niimgs=subjects_niimgs[i],
+    #         design_matrices=encoder.design_matrices_[i],
+    #         contrasts=sanitize_contrast(docs[i]['contrasts'], per_run=True))
+    #         for i, subject_id in enumerate(infos['subjects'])
+    #     )
